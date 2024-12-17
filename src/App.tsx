af99f7938124aa3b83f0ce101e704fed0941a154
severinlindenmann <github@severin.io>
2024-12-17 17:30:09 +0100

    basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
+import BasicAppShell from "./components/BasicAppShell";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return <MantineProvider theme={theme}>
+
+    <BasicAppShell />
+
+  </MantineProvider>;
 }
